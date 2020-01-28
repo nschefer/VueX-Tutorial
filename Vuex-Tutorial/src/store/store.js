5be0dcebba756291ac5cbaabb9a5dfbc5f115cc1
Noah Schefer <nbschefer@gmail.com>
2020-01-28 13:20:27 -0500

    Lesson 5 Complete

--- Vuex-Tutorial/src/store/store.js
+++ Vuex-Tutorial/src/store/store.js
@@ -11,5 +11,18 @@ export const store = new Vuex.Store({
       { id: 2, name: 'Green Shells', price: 60 },
       { id: 3, name: 'Red Shells', price: 80 },
     ]
+  },
+  getters: {
+    saleProducts: state => {
+      const saleProducts = state.products.map(product => {
+        return {
+          id: product.id,
+          name: `**${product.name}**`,
+          price: product.price / 2,
+        };
+      });
+      return saleProducts;
+    },
   }
-})
+}
+)
