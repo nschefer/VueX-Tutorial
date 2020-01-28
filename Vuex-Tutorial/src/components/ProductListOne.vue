adedfd6c8c7c4c1ea77bb5f0bb95eec0280020d0
Noah Schefer <nbschefer@gmail.com>
2020-01-28 12:43:26 -0500

    Lesson 3 Complete

--- Vuex-Tutorial/src/components/ProductListOne.vue
+++ Vuex-Tutorial/src/components/ProductListOne.vue
@@ -2,7 +2,7 @@
   <div id="product-list-one">
     <h2>Product List One</h2>
     <ul>
-      <li v-for="product in products">
+      <li v-for="product in products" :key="product.id">
         <span class="name">{{product.name}}</span>
         <span class="price">${{product.price}}</span>
       </li>
--- Vuex-Tutorial/src/components/ProductListTwo.vue
+++ Vuex-Tutorial/src/components/ProductListTwo.vue
@@ -2,7 +2,7 @@
   <div id="product-list-two">
     <h2>Product List Two</h2>
     <ul>
-      <li v-for="product in products">
+      <li v-for="product in products" :key="product.id">
         <span class="name">{{product.name}}</span>
         <span class="price">${{product.price}}</span>
       </li>
